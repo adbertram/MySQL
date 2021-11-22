8b0e48a3d5d9e8c0024333f044d3f66cfa7f4eb2
Rob Taylor <rob@mpcs.com>
2021-11-21 21:11:59 -0500

    Fixed Manifest and Export Issue

--- MySQL.psm1
+++ MySQL.psm1
@@ -1,24 +1,22 @@
 Set-StrictMode -Version Latest
 
-$null = [System.Reflection.Assembly]::LoadWithPartialName('MySql.Data')
+#$null = [System.Reflection.Assembly]::LoadWithPartialName('MySql.Data')
 
 $pubpath=$PSScriptRoot + "\public\"
 $publist=Get-ChildItem -Path $pubpath -Filter "*.ps1" -Name
 $prvpath=$PSScriptRoot + "\private\"
 $prvlist=Get-ChildItem -Path $prvpath -Filter "*.ps1" -Name
 
-
 foreach ($func in $publist)
 {
-    Write-Verbose "Loading Function: $func"
     $funcname=$func -replace ".ps1",""
     . ($pubpath + $func)
-    Write-Verbose "Function $funcname Exported"
     Export-ModuleMember -Function $funcname
 }
+
 
 foreach ($func in $prvlist)
 {
     . ($prvpath + $func)
-    Write-Verbose "Function $funcname Loaded - Private"
+    $funcname=$func -replace ".ps1",""
 }
